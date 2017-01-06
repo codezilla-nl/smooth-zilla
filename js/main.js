30945253497bd576e0693bb4ec5949a13e10293b
Patrick Kraaij <patrick.kraaij@gmail.com>
2017-01-06 13:44:53 +0100

    Adjust some code, need some help from fellow colleague

--- js/main.js
+++ js/main.js
@@ -9,43 +9,39 @@
         return false;
     }    
 
-    function whichTransitionEvent(){
-        var t;
+    function whichTransitionEvent() {
         var el = document.createElement('fakeelement');
         var transitions = {
-        'transition':'transitionend',
-        'OTransition':'oTransitionEnd',
-        'MozTransition':'transitionend',
-        'WebkitTransition':'webkitTransitionEnd'
-        }
+            'transition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'MozTransition': 'transitionend',
+            'WebkitTransition': 'webkitTransitionEnd'
+        };
 
-        for(t in transitions){
-            if( el.style[t] !== undefined ){
+        for(var t in transitions) {
+            if(el.style[t] !== undefined) {
                 return transitions[t];
             }
         }
     }
 
     function navigateTo(href, pushState) {
-        var container = document.getElementById('container')
 
         function swap(html, href) {
-
+			var container = document.getElementById('container');
             var transitionEvent = whichTransitionEvent();
-            console.log('transitionEvent', transitionEvent);
             
             transitionEvent && container.addEventListener(transitionEvent, function() {
                 document.write(html);
                 document.close();
 
-                var container = document.getElementById('container')
+                var container = document.getElementById('container');
                 container.setAttribute('class', 'fade');
 
                 // Barf, just for demo purposes
                 setTimeout(function() {
                     container.setAttribute('class', 'fade in');
                 }, 100);
-
             });
       
             container.setAttribute('class', 'fade out');
@@ -55,21 +51,12 @@
             }
         }
 
-        
-    
-
         if (!pagesHtml[href]) {
-            document.body.style.cursor = 'progress';
-
             loadPage(href, function(page) {
-                document.body.style.cursor = 'default';
-
-                swap(page, href)
-            })
+                swap(page, href);
+            });
         } else {
-            // Loading from cache!
-            console.log('loading from cache!');
-            
+            // loading from cache
             swap(pagesHtml[href], href);
         }
     }
@@ -78,7 +65,7 @@
         var xmlhttp = new XMLHttpRequest();
 
         xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
                 if (xmlhttp.status == 200) {
                     cb(xmlhttp.responseText);
                 }
@@ -91,7 +78,7 @@
             }
         };
 
-        xmlhttp.open("GET", href, true);
+        xmlhttp.open('GET', href, true);
         xmlhttp.send();
     }
 
@@ -100,39 +87,40 @@
 
         for (var i = 0, len = links.length; i < len; i++) {
             if (!isExternal(links[i].getAttribute('href'))) {
-                console.log('ohai', links[i]);
+                // console.log('ohai', links[i]);
 
                 pages.push(links[i].getAttribute('href'));
                 
                 links[i].addEventListener('click', function(event) {
-
                     navigateTo(this.getAttribute('href'), true);
                     event.preventDefault();
-                    
                 });
             }
         }
-        
     }
 
     function loadBrowserNavListener() {
         window.addEventListener('popstate', function() {
             navigateTo(location.pathname, false);
-        })
+        });
+    }
+
+    function serviceWorker() {
+        if (window.Worker) {
+            var myWorker = new Worker('js/worker.js');
+            myWorker.postMessage(pages);
+
+            myWorker.onmessage = function(e) {
+                // console.log('Message received from worker', e.data);
+                pagesHtml[e.data.url] = e.data.html;
+            };
+        }
+        else {
+            throw Error('Too bad, no service worker support');
+        }
     }
 
     loadLinkListener();
     loadBrowserNavListener();
-
-    if (window.Worker) {
-
-        var myWorker = new Worker('js/worker.js');
-        myWorker.postMessage(pages);
-
-        myWorker.onmessage = function(e) {
-            console.log('Message received from worker', e.data);
-            pagesHtml[e.data.url] = e.data.html;
-        }
-    }
-
+    serviceWorker();
 })();
--- js/worker.js
+++ js/worker.js
@@ -1,17 +1,3 @@
-onmessage = function(e) {
-  console.log('Message received from main script');
-  console.log(e.data);
-
-  e.data.forEach(function(page) {
-      loadPage('/' + page, function(html) {
-          postMessage({
-              url: page,
-              html: html
-          });
-      })
-  });
-}
-
 function loadPage(href, cb) {
     var xmlhttp = new XMLHttpRequest();
 
@@ -19,6 +5,7 @@ function loadPage(href, cb) {
         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
             if (xmlhttp.status == 200) {
                 cb(xmlhttp.responseText);
+                // console.log(xmlhttp.responseText);
             }
             else if (xmlhttp.status == 400) {
                 throw Error('There was an error 400');
@@ -31,4 +18,18 @@ function loadPage(href, cb) {
 
     xmlhttp.open("GET", href, true);
     xmlhttp.send();
+}
+
+onmessage = function(e) {
+//   console.log('Message received from main script');
+//   console.log(e.data);
+
+  e.data.forEach(function(page) {
+      loadPage('/' + page, function(html) {
+          postMessage({
+              url: page,
+              html: html
+          });
+      })
+  });
 }
